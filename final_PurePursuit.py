e84b98a54abd6b745f2603edfe7d99cac7591e69
PakornPK <sweetie_jimmie@hotmail.com>
2018-12-12 00:58:23 +0700

    eld/l

--- final_PurePursuit.py
+++ final_PurePursuit.py
@@ -11,12 +11,13 @@ wn.title('Pure Pursuit')
 setup(500,500)
 bgcolor('black')
 mode("standard")
-poly = ((18,0),(-18,0),(-18,52),(18,52))
+poly = ((18,0),(-18,0),(-18,32),(0,52),(18,32))
 s = Shape("compound")
 s.addcomponent(poly,"yellow","red")
 register_shape("car", s)
 hideturtle()
 shape("car")
+
 pen(fillcolor="black", pencolor="blue", pensize=5)
 pensize(3)
 penup()
@@ -26,17 +27,16 @@ goto(150,0)
 penup()
 
 #start 
-start_point = [0,0]
+start_point = [50,50]
 goto(start_point)
 path = np.array([[-150,0],[150,0]])
-stop_point = [150,0]
-seth(180)
+stop_point = [-150,0]
+seth(220)
 speed(7)
 showturtle()
 
-
-
-while True: 
+for i in range(1):
+#while True: 
         #step 1 
         pos_car = pos() #(x,y) current position 
         angle_H = heading()
@@ -78,62 +78,51 @@ while True:
         vecP = P*np.array(vecA)
         vertical_X = vecP[0]+nearPoint[0]
         vertical_Y = vecP[1]+nearPoint[1]
+        verticalPoint = [vertical_X,vertical_Y]
         print('Vector A is ',vecA)
         print('Vector B is ',vecB)
         print('Vector P is ',vecP)
         print('Vertical Point is ({0:.3f},{1:.3f})'.format(vertical_X,vertical_Y))
 
         #step 4 
-        #find the cut point with circle cut line 
-        L = 52
-        y = 0 #equation of path 
-        x1 = -1*np.sqrt(-y**2 + L**2) + pos_car[0] 
-        x2 = np.sqrt(-y**2 + L**2) + pos_car[0]  
-        print('Cut Point is ({0:.3f},{1:.3f})'.format(x1,x2))
+        #find shortate distance 
+        shortate = np.linalg.norm(np.array(verticalPoint)-np.array(pos_car))
+        np.array(shortate)
+        print("Shortate distance = ",shortate)
+
+        #fix V and Goal  
+        Goal = [vertical_X - 50,verticalPoint[1]]
+        print("Pos = ",pos())
+        print("goal = ",Goal)
+
+        #step 5 
+        #find alpha 
+        L = np.linalg.norm(np.array(Goal)-np.array(pos()))  
+        eld = np.linalg.norm(np.array(Goal)-np.array(frontCar))
+        alpha = np.arcsin(eld/L)
+        #r = (L**2)/(2*shortate)
+        K = (2*np.sin(alpha))/L
+        V = 100
+        angle_H = heading()
+        print('heading before calculate = ',angle_H)
+        defX = V*np.cos(math.radians(angle_H))
+        defY = V*np.sin(math.radians(angle_H))
+        Zixmar = np.arctan(K*52)
+        defPhi = (V/52)*(np.tan(Zixmar))
+        print("L =",L)
+        #print("R =",r)
+        print("DEF X = ",defX)
+        print("DEF Y = ",defY)
+        print("DEF PHI = ",defPhi)
         
-        #step 5
-        if np.linalg.norm(np.array([x1,0])-stop_point) < np.linalg.norm(np.array([x2,0])-stop_point) :
-                goalPoint = [x1,y] 
-        else:
-                goalPoint = [x2,y] 
-        print('Select Goal Point = ',goalPoint)
-
-        #step 6 
-        try :
-                ld = np.linalg.norm(pos_car-goalPoint)
-                eld = np.linalg.norm(frontCar-goalPoint)
-                print('eld = {0:.3f} ld = {1:.3f}'.format(eld,ld))
-                if eld > ld : 
-                        eld = ld   
-                        
-                Alpha = np.arcsin(eld/ld)
-                
-
-                V =2
-                Zixmar = math.degrees(((V*L)*np.sin(Alpha))/ld)
-                Theta = (V/L)*np.tan(Zixmar)
-               
-                
-        except ValueError : 
-                print('NaN')
-
-        #step 7 
-        speed(1)
-        print('Zixmar =',Zixmar)
-        if pos_car[1] > 0 : 
-                angleCar = 180+Zixmar
-        else:
-                angleCar = 180-Zixmar
-        seth(angleCar)
-        print('Heading =',heading())
-        pencolor('green')
-        pendown()
-        fd(1)
-        print('Current position =',pos())
-        if (pos_car[0] - stop_point[0]) >= -15 and (pos_car[0] - stop_point[0]) <= 15 and (pos_car[1] - stop_point[1]) >= -15 and (pos_car[1] - stop_point[1]) <= 15:
-                print((pos_car[0] - stop_point[0]))
-                break
-        print("-----------------------")
-
+        #step 6
+        angle_H = angle_H + defPhi 
+        posX = pos_car[0] + defX
+        posY = pos_car[1] + defY
+        seth(angle_H)
+        goto(posX,posY)
+        print("new Pos = ",pos())
+        print('new Heading =',heading())
+        print('-------------------',i,'------------------------------')
 
 done()
