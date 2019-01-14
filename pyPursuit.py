40c9ad9747b63bb4366e6d75513c1c7155c481ed
PakornPK <sweetie_jimmie@hotmail.com>
2019-01-14 17:00:36 +0700

    fronCar

--- pyPursuit.py
+++ pyPursuit.py
@@ -7,9 +7,9 @@ import math
 class pyPursuit(tkinter.Frame):
     def __init__(self, master): 
         super(pyPursuit, self).__init__(master)
-        self.angle = 0 
-        self.poly = self.createCarBot(200,450,self.angle)
-        self.center = self.find_center(self.poly)
+        self.angle = -90 
+        self.center = [200,450]
+        self.poly = self.createCarBot(self.center[0],self.center[1],self.angle)
         self.shortate = np.linalg.norm(self.center[0]-250) # path fig 
         self.LH = 100
         self.grid()
@@ -24,7 +24,7 @@ class pyPursuit(tkinter.Frame):
         textPathTop = self.CV.create_text(270,10,text='(250,0)')
         textPathBottom = self.CV.create_text(275,490,text='(250,500)')
         textSh = self.CV.create_text(50,20,text = 'Shortate = ' + str(self.shortate))
-        textLH = self.CV.create_text(55,35,text = 'Look ahead = 80.0')
+        textLH = self.CV.create_text(55,35,text = 'Look ahead = {}'.format(self.LH))
 
         ct = np.array(self.center)
         lh = np.array([250.0,self.center[1]-self.LH])
@@ -33,7 +33,7 @@ class pyPursuit(tkinter.Frame):
         R = np.square(L)/(2*self.shortate)
         D = R-self.shortate 
         textR = self.CV.create_text(35,65,text = 'R = {0:.3f}'.format(R))
-        textSpeed = self.CV.create_text(42,80, text = 'Speed = 2 m/s')
+        textSpeed = self.CV.create_text(44,80, text = 'Speed = 2 m/s')
 
         self.poly = self.rotate(self.poly,self.angle,tuple(self.center))
         self.carBot = self.CV.create_polygon(self.poly, fill = 'yellow' ,outline = 'black')
@@ -45,11 +45,24 @@ class pyPursuit(tkinter.Frame):
         textCenterR = self.CV.create_text(250+D,self.center[1],text = '({0},{1})'.format(250+D,self.center[1]))
         circle = self.create_circle(250+D,self.center[1],R,self.CV)
 
-        for i in range(10): 
+        fc_x = math.cos(math.radians(self.angle))
+        fc_y = math.sin(math.radians(self.angle)) 
+        fc = 30 * np.array([fc_x,fc_y])
+        frontCar = np.add(self.center,fc)
+        print(frontCar)
+        lineFront = self.CV.create_line(frontCar[0],frontCar[1],lh[0],lh[1],fill = 'red')
+        ld = L 
+        eld = np.linalg.norm(frontCar-lh)
+        alpha = math.degrees(np.arcsin(eld/ld))
+        textELD = self.CV.create_text(35,95, text= 'eld = {:.3f}'.format(eld))
+        textAlpha = self.CV.create_text(30,110, text= 'α = {:.3f}'.format(alpha))
+        """
+        for i in range(10):
+            self.CV.tag_raise(self.carBot) 
             self.CV.move(self.carBot,10,-10)
             time.sleep(1)
             self.CV.update()
-
+        """
 
     def create_circle(self,x, y, r, canvasName): 
         x0 = x - r
@@ -59,7 +72,7 @@ class pyPursuit(tkinter.Frame):
         return canvasName.create_oval(x0, y0, x1, y1, outline = 'green')
 
     def createCarBot(self,x,y,angle):  
-        Carbot = [[x-10,y-30],[x+10,y-30],[x+10,y],[x-10,y]]
+        Carbot = [[x,y-10],[x+30,y-10],[x+30,y+10],[x,y+10]]
         return Carbot
         
     def rotate(self, points, angle, center): 
@@ -75,13 +88,6 @@ class pyPursuit(tkinter.Frame):
             y_new = x_old * sin_val + y_old * cos_val
             new_points.append([x_new + cx, y_new + cy])
         return new_points
-
-    def find_center(self,old_point):
-        point = np.array(old_point)
-        X = np.linalg.norm(point[0][0]-point[1][0])/2 
-        Y = np.linalg.norm(point[0][1]-point[1][1])/2  
-        center = [point[3][0]+X,point[3][1]+Y]     
-        return center 
 
 
 
