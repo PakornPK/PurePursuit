f451e187a16a1ae8dcd77413e51cdcbdf2132caf
PakornPK <sweetie_jimmie@hotmail.com>
2019-01-16 00:06:04 +0700

    os

--- testThread.py
+++ testThread.py
@@ -1,23 +1,44 @@
-import threading
-
-class MyThread(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.sleep_event = threading.Event()
-        
-
-    def run(self):
-        while True:
-            self.sleep_event.clear()
-            self.sleep_event.wait(60)
-            threading.Thread(target=self._run).start()
-
-    def _run(self):
-        print('run')
-
-if __name__ == "__main__":
-    
-    my_thread = MyThread()
-    my_thread.start()
-
+import os
+import tkinter
+ 
+UPDATE_RATE = 1000
+ 
+class Application(tkinter.Frame):
+    """ GUI """
+ 
+    def __init__(self, master):
+        """ Initialize the Frame"""
+        tkinter.Frame.__init__(self, master)
+        self.grid()
+        self.create_widgets()
+        self.updater()
+ 
+    def create_widgets(self):
+        """Create button. """
+#         import os # import at top
+#         import subprocess # doing nothing
+        # Router
+        self.button1 = tkinter.Button(self)
+        self.button1["text"] = "Router"
+        self.button1["fg"] = "white"
+        self.button1.grid(row=0, column=5, rowspan=1, columnspan=2)
+ 
+    def update_button1(self):
+        # Ping
+        hostname = "192.168.1.1"
+        response = os.system("ping -n 1 " + hostname)
+        # response
+        if response == 0:
+            self.button1["bg"] = "green"
+        else:
+            self.button1["bg"] = "red"
+ 
+    def updater(self):
+        self.update_button1()
+        self.after(UPDATE_RATE, self.updater)
+ 
+root = tkinter.Tk()
+root.title("monitor")
+root.geometry("500x500")
+app = Application(root)
+root.mainloop()
