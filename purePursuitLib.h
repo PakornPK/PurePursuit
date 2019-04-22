127de2dbc44d3efb3202180e930a4c7b96a36efc
Pakorn <32038814+PakornPK@users.noreply.github.com>
2019-04-23 00:00:05 +0700

    Create purePursuitLib.h

---
+++ purePursuitLib.h
@@ -0,0 +1,13 @@
+#include <math.h>
+
+using namespace std;
+
+class pathCal{
+    public:
+    double norm(double *pointA[2], double *pointB[2]); 
+    double radianCal(double L, double shortate);
+    double alpha(double ld, double eld); 
+    double KFactor(double alpha, double ld);
+    double zixma(double k); 
+};
+
