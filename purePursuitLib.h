1d007cd24414ecc6b2308b4da23e000be496916a
Pakorn <32038814+PakornPK@users.noreply.github.com>
2019-04-24 00:07:43 +0700

    Update purePursuitLib.h

--- purePursuitLib.h
+++ purePursuitLib.h
@@ -4,7 +4,7 @@ using namespace std;
 
 class pathCal{
     public:
-    double norm(double pointA[2], double pointB[2]); 
+    double norm(double *pointA[2], double *pointB[2]); 
     double radianCal(double L, double shortate);
     double alpha(double ld, double eld); 
     double KFactor(double alpha, double ld);
